bddf84cb283fcfba16aa459e9a6256ab5c875dc1
Belal <mohammadvns121@gmail.com>
2024-02-04 11:01:30 +0530

    set up utils custome api response models

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    // console.log(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    const connectionIntances = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MOngoDB connected !! DB HOST: ${connectionIntances.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB CONNECTION FAILD>>>", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
+
+// process is come from nodeJs
